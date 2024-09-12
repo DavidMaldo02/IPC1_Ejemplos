38708329e81b31458e8c20ab9f8c979d328e0060
DavidMaldo02 <dvid2016@hotmail.com>
2024-09-12 12:09:55 -0600

    procesado de productos

---
+++ src/main/java/ipc1/lab/models/MyThread.java
@@ -0,0 +1,57 @@
+package ipc1.lab.models;
+
+import javax.swing.*;
+
+public class MyThread extends Thread {
+    Product product;
+    JProgressBar progressBar;
+    String line;
+
+    public MyThread(Product product, JProgressBar progressBar, String line) {
+        this.product = product;
+        this.progressBar = progressBar;
+        this.line = line;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public JProgressBar getProgressBar() {
+        return progressBar;
+    }
+
+    public void setProgressBar(JProgressBar progressBar) {
+        this.progressBar = progressBar;
+    }
+
+    public String getLine() {
+        return line;
+    }
+
+    public void setLine(String line) {
+        this.line = line;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Inicio hilo " + this.line);
+        int fraction = 100 / product.getProcessTime();
+        int progress = 0;
+        for (int i = 0; i < product.getProcessTime(); i++) {
+            try {
+                Thread.sleep(1000);
+                progress += fraction;
+                progressBar.setValue(progress);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        progressBar.setValue(100);
+        System.out.println("Fin hilo " + this.line);
+    }
+}
--- src/main/java/ipc1/lab/views/ResearcherFrame.java
+++ src/main/java/ipc1/lab/views/ResearcherFrame.java
@@ -1,6 +1,8 @@
 package ipc1.lab.views;
 
 import ipc1.lab.controllers.UserController;
+import ipc1.lab.models.MyThread;
+import ipc1.lab.models.Product;
 import ipc1.lab.ui.Fonts;
 import ipc1.lab.ui.Pallete;
 
@@ -9,8 +11,11 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
 
 public class ResearcherFrame extends JFrame {
+    ArrayList<Product> products = new ArrayList<>();
+
     public ResearcherFrame() throws HeadlessException {
         setTitle("Investigador");
         setSize(new Dimension(750, 500));
@@ -24,9 +29,11 @@ public class ResearcherFrame extends JFrame {
                 new LoginFrame().setVisible(true);
             }
         });
+        // Layout
+        GridLayout gridLayout = new GridLayout(5, 1, 0, 24);
 
         // Panel
-        JPanel loginPanel = new JPanel();
+        JPanel loginPanel = new JPanel(gridLayout);
         loginPanel.setBackground(Pallete.background);
         loginPanel.setBorder(new EmptyBorder(32, 32, 32, 32));
         this.setContentPane(loginPanel);
@@ -37,6 +44,43 @@ public class ResearcherFrame extends JFrame {
         title.setFont(Fonts.title);
         title.setHorizontalAlignment(SwingConstants.CENTER);
         this.add(title);
-        setVisible(true);
+
+        JProgressBar progressBar1 = new JProgressBar(0, 100);
+        this.add(progressBar1);
+        JProgressBar progressBar2 = new JProgressBar(0, 100);
+        this.add(progressBar2);
+        JProgressBar progressBar3 = new JProgressBar(0, 100);
+        this.add(progressBar3);
+
+        this.setVisible(true);
+
+        Product product = new Product("P-01", "Carro", "Aluminio", "Verde", 5);
+        products.add(product);
+        products.add(product);
+        products.add(product);
+        progressBar1.setStringPainted(true);
+        progressBar2.setStringPainted(true);
+        progressBar3.setStringPainted(true);
+
+        for (int i = 0; i < products.size(); i++) {
+            try {
+                progressBar1.setValue(0);
+                progressBar2.setValue(0);
+                progressBar3.setValue(0);
+                MyThread myThread1 = new MyThread(products.get(i), progressBar1, "ensamblaje");
+                MyThread myThread2 = new MyThread(products.get(i), progressBar2, "pintura");
+                MyThread myThread3 = new MyThread(products.get(i), progressBar3, "empaquetado");
+                myThread1.start();
+                myThread1.join();
+                myThread2.start();
+                myThread2.join();
+                myThread3.start();
+                myThread3.join();
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+
     }
 }
